b10d7352d6e04722fae54cf404d2c483dc2322b3
Ricardo <rpires@rpiredev.site>
2022-01-30 12:45:42 +0100

    🧙🏾‍♂️ tailwind setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--
+    This example requires updating your template:
+
+    ```
+    <html class="h-full">
+    <body class="h-full">
+    ```
+  -->
+  <div class="bg-white min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+    <div class="max-w-max mx-auto">
+      <main class="sm:flex">
+
+        <div class="sm:ml-6">
+          <div class="sm:border-gray-200 sm:pl-6">
+            <h1 class="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">VUE3 - TAILWIND WIREFRAME</h1>
+            <p class="mt-1 text-base text-gray-500">Created with ❤️ by Ricardo Pires.</p>
+            <p class="mt-1 text-base text-gray-500">This Project includes Vue 3, Router, Vuex, tailwind, headless ui, heroicons </p>
+          </div>
+          <div class="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+          
+          </div>
+        </div>
+      </main>
+    </div>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
